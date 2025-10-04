6dd2467b07f2bd4c14ebe8cbef02820611772dde
HarshithKoriRaj <harshith.02.14@gmail.com>
2025-10-04 12:44:02 -0400

    ALLOWED_HOSTS

--- newproject/settings.py
+++ newproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ote)mm9438e(i42e@nj9-(!^jhu3&96n+anun!*-4#!$8ul@-+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".elasticbeanstalk.com"]
 
 
 # Application definition
